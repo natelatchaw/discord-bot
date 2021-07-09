7b8d1e6914bef09fa198f7b817db0401105baed7
Nathan Latchaw <natelatchaw@gmail.com>
2021-07-08 20:43:07 -0500

    update start script

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,26 @@ import dotenv from 'dotenv';
 import { URL } from 'url';
 import { Console } from './console';
 import { Core } from './core';
+import { File } from './models/file';
+import { PathLike, promises } from 'fs';
 
-dotenv.config();
-const endpoint: URL = new URL('wss://gateway.discord.gg/?v=9&encoding=json');
-const token: string = process.env.TOKEN ?? '';
-Console.log(`Token: ${token}`);
-const core: Core = new Core(endpoint, token);
+async function start() {
+    try {
+        const file: File = new File('./.env');
+        await file.open();
+        await file.close();
+    }
+    catch (error: any) {
+        console.error(error);
+    }
+    
+    dotenv.config();
+    const endpoint: URL = new URL('wss://gateway.discord.gg/?v=9&encoding=json');
+    const token: string = process.env.TOKEN ?? '';
+    Console.log(`Token: ${token}`);
+    const core: Core = new Core(endpoint, token);
+}
+
+start()
+    .then(() => console.log('Complete.'))
+    .catch((error: Error) => console.error(error));
