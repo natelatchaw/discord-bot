6e51649799ee1acfd25b3fa1e8238b58905a516c
Nate Latchaw <natelatchaw@gmail.com>
2021-07-10 19:30:23 -0500

    changed index to export manifest

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,52 @@
-import dotenv from 'dotenv';
-import { URL } from 'url';
-import { Console } from './console';
-import { Core } from './core';
-import { File } from './models/file';
+// ./client
+export { Client } from './client';
 
-/**
- * Open the connection to Discord
- */
-async function start() {
-  try {
-    const file: File = new File('./.env');
-    await file.open();
-    await file.close();
-  }
-  catch (error: any) {
-    if (error.code === 'EEXIST') { } else throw error;
-  }
+// ./console
+export { Console } from './console';
 
-  dotenv.config();
-  const endpoint: URL = new URL('wss://gateway.discord.gg/?v=9&encoding=json');
-  const token: string = process.env.TOKEN ?? '';
-  Console.log(`Token: ${token}`);
-  const core: Core = new Core(endpoint, token);
-  console.log(core);
-}
+// ./core
+export { Core } from './core';
 
-start()
-    .then(() => console.log('Complete.'))
-    .catch((error: Error) => console.error(error));
+// ./models
+export { File } from './models/file';
+export { Payload } from './models/payload';
+
+// ./models/events
+export { GuildCreate } from './models/events/guildCreate';
+export { Ready } from './models/events/ready';
+
+// ./models/payloads
+export { Dispatch } from './models/payloads/dispatch';
+export { Heartbeat } from './models/payloads/heartbeat';
+export { HeartbeatACK } from './models/payloads/heartbeatACK';
+export { Hello } from './models/payloads/hello';
+export { Identify } from './models/payloads/identify';
+export { Resume } from './models/payloads/resume';
+
+// ./models/resources
+export { Activity } from './models/resources/activity';
+export { ActivityAssets } from './models/resources/activityAssets';
+export { ActivityButton } from './models/resources/activityButtons';
+export { ActivityEmoji } from './models/resources/activityEmoji';
+export { ActivityParty } from './models/resources/activityParty';
+export { ActivitySecrets } from './models/resources/activitySecrets';
+export { Application } from './models/resources/application';
+export { Channel } from './models/resources/channel';
+export { ClientStatus } from './models/resources/clientStatus';
+export { Emoji } from './models/resources/emoji';
+export { Feature } from './models/resources/feature';
+export { Guild } from './models/resources/guild';
+export { Member } from './models/resources/member';
+export { Overwrite } from './models/resources/overwrite';
+export { Prescence } from './models/resources/presence';
+export { Role } from './models/resources/role';
+export { Snowflake } from './models/resources/snowflake';
+export { Stage } from './models/resources/stage';
+export { Team } from './models/resources/team';
+export { TeamMembers } from './models/resources/teamMembers';
+export { ThreadMember } from './models/resources/threadMember';
+export { ThreadMetadata } from './models/resources/threadMetadata';
+export { Timestamps } from './models/resources/timestamps';
+export { User } from './models/resources/user';
+export { WelcomeScreen } from './models/resources/welcomeScreen';
+export { WelcomeScreenChannel } from './models/resources/welcomeScreenChannel';
